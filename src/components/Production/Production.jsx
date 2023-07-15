f1cfcc2ea8f34ff76b5b0b2c9909cb75f6d56f5f
Vladislav Chornyi <98426939+VladyslavIT@users.noreply.github.com>
2023-07-15 20:20:59 +0300

    add logic

--- src/components/Production/Production.jsx
+++ src/components/Production/Production.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { TfiSearch } from 'react-icons/tfi';
 import {
   Section,
@@ -12,22 +12,54 @@ import Modal from 'components/Modal/Modal';
 
 const Production = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [text, setText] = useState('');
+  const [letter, setLetter] = useState([]);
+
+  useEffect(() => {
+    const findUniqueLetter = () => {
+      const arr = text.toLowerCase().split(/\s+/);
+      const uniqueLetters = [];
+
+      for (const item of arr) {
+        let uniqueChar = item[0];
+        let isDuplicate = false;
+
+        for (let i = 1; i < item.length; i += 1) {
+          if (item[i] === uniqueChar) {
+            isDuplicate = true;
+            break;
+          }
+        }
+        if (!isDuplicate) {
+          uniqueLetters.push(uniqueChar);
+        }
+      }
+      return uniqueLetters;
+    };
+    setLetter(findUniqueLetter());
+  }, [text]);
+
+  console.log(letter);
+
+  const handleChange = ({ target: { value } }) => {
+    setText(value);
+  };
 
   const openModal = () => {
     setIsOpen(true);
   };
   const closeModal = () => {
     setIsOpen(false);
-  }
-  
+  };
+
   return (
     <>
       <Section>
         <Title>LetterTracker</Title>
-        <Modal isOpen={isOpen} onClose={closeModal}/>
+        <Modal isOpen={isOpen} onClose={closeModal} />
         <SubTitle>Enter text here</SubTitle>
-        <TextArea></TextArea>
-       
+        <TextArea onChange={handleChange}></TextArea>
+
         <Button onClick={openModal}>
           <BtnInner>
             Search <TfiSearch />
